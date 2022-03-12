fad71b40e8a562a04fbc1fa1b4e279b45548c6ca
SUMMER988 <summmer206@gmail.com>
2022-03-12 10:57:26 +0800

    JAVA

--- chapter10/src/com/sun/homework/Homework03.java
+++ chapter10/src/com/sun/homework/Homework03.java
@@ -21,12 +21,6 @@ public class Homework03 {
         for (Animal animal : animals) {
             animal.shout();
         }
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i);
-        }
-        for (int i = 0; i < 50; i++) {
-            System.out.println(i);
-        }
 
     }
 }
