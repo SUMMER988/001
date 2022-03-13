7f59adb5b405fe934dbc3f48b1e0830ec2d3fcfa
SUMMER988 <summmer206@gmail.com>
2022-03-14 00:39:49 +0800

    ArrayIndexOutOfBoundsException_

---
+++ chapter12/src/com/sun/exception_/ArrayIndexOutOfBoundsException_.java
@@ -0,0 +1,13 @@
+package com.sun.exception_;
+
+/**
+ * ClassName: ArrayIndexOutOfBoundsException_
+ * Description:
+ * date: 2022/3/14 0:39
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class ArrayIndexOutOfBoundsException_ {
+}
