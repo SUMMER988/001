abde4a61c987eb1ec91778f9a533aac7df9c403e
SUMMER988 <summmer206@gmail.com>
2022-03-14 00:33:37 +0800

    NullPointerException_

---
+++ chapter12/src/com/sun/exception_/NullPointerException_.java
@@ -0,0 +1,13 @@
+package com.sun.exception_;
+
+/**
+ * ClassName: NullPointerException_
+ * Description:
+ * date: 2022/3/14 0:33
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class NullPointerException_ {
+}
