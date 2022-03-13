bc6935948c5646b9507f57e7a18a6971dd6aa33a
SUMMER988 <summmer206@gmail.com>
2022-03-14 00:50:05 +0800

    ArrayIndexOutOfBoundsException_

--- chapter12/src/com/sun/exception_/ArrayIndexOutOfBoundsException_.java
+++ chapter12/src/com/sun/exception_/ArrayIndexOutOfBoundsException_.java
@@ -10,4 +10,10 @@ package com.sun.exception_;
  * @since JDK 1.8
  */
 public class ArrayIndexOutOfBoundsException_ {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4};
+        for (int i = 0; i <= arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
