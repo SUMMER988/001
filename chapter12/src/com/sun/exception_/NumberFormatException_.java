14d8df0195cd9613246e60dd0a1494839d3f3a80
SUMMER988 <summmer206@gmail.com>
2022-03-14 01:00:30 +0800

    Exception_

--- chapter12/src/com/sun/exception_/NumberFormatException_.java
+++ chapter12/src/com/sun/exception_/NumberFormatException_.java
@@ -10,4 +10,11 @@ package com.sun.exception_;
  * @since JDK 1.8
  */
 public class NumberFormatException_ {
+    public static void main(String[] args) {
+        String name = "韩顺平教育";
+        //将String 转成 int
+        int num = Integer.parseInt(name);
+        //抛出NumberFormatException
+        System.out.println(num);//1234
+    }
 }
