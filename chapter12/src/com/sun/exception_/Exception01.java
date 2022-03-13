a9caa0949a6ba2f151d3d1243dab93d5272063a9
SUMMER988 <summmer206@gmail.com>
2022-03-13 23:25:32 +0800

    out

---
+++ chapter12/src/com/sun/exception_/Exception01.java
@@ -0,0 +1,16 @@
+package com.sun.exception_;
+
+/**
+ * ClassName: Exception01
+ * Description:
+ * date: 2022/3/13 23:22
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class Exception01 {
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+}
