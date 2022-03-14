acdb90c5dc3a0d16499a9051a378bd7911ae171a
SUMMER988 <summmer206@gmail.com>
2022-03-14 19:53:55 +0800

    Exception_

--- chapter12/src/com/sun/try_/TryCatchDetail02.java
+++ chapter12/src/com/sun/try_/TryCatchDetail02.java
@@ -11,21 +11,32 @@ package com.sun.try_;
  */
 public class TryCatchDetail02 {
     public static void main(String[] args) {
+
+        //老韩解读
+        //1.如果try代码块有可能有多个异常
+        //2.可以使用多个catch 分别捕获不同的异常，相应处理
+        //3.要求子类异常写在前面，父类异常写在后面
         try {
             Person person = new Person();
-            person = null;
-            System.out.println(person.getName());
+            //person = null;
+            System.out.println(person.getName());//NullPointerException
+            int n1 = 10;
+            int n2 = 0;
+            int res = n1 / n2;//ArithmeticException
         } catch (NullPointerException e) {
+            System.out.println("空指针异常=" + e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println("算术异常=" + e.getMessage());
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
-            System.out.println("程序执行中...");
+            System.out.println("继续执行...");
         }
-        System.out.println("程序继续执行...");
     }
 }
 
 class Person {
-    private String name = "sun";
+    private String name = "jack";
 
     public String getName() {
         return name;
