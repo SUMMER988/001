7c496216db59b45a8209b28ebfef075b8393aa3b
SUMMER988 <summmer206@gmail.com>
2022-03-15 16:26:48 +0800

    Exception_

---
+++ chapter12/src/com/sun/homework/Homework03.java
@@ -0,0 +1,31 @@
+package com.sun.homework;
+
+/**
+ * ClassName: Homework03
+ * Description:
+ * date: 2022/3/15 16:20
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class Homework03 {
+    public static void func() {//静态方法
+        try {
+            throw new RuntimeException();
+        } finally {
+            System.out.println("B");
+        }
+    }
+
+    public static void main(String[] args) {//main方法
+        try {
+            func();
+            System.out.println("A");//try代码块中抛出异常，剩余语句不执行
+        } catch (Exception e) {
+            System.out.println("C");
+        }
+        System.out.println("D");
+    }
+
+}
