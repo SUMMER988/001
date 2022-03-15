17ec8ccff8efb8c636cdff70e014174992bf1043
SUMMER988 <summmer206@gmail.com>
2022-03-15 20:23:38 +0800

    wrapper

---
+++ chapter13/src/com/sun/string_/StringExercise01.java
@@ -0,0 +1,19 @@
+package com.sun.string_;
+
+/**
+ * ClassName: StringExercise01
+ * Description:
+ * date: 2022/3/15 20:22
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class StringExercise01 {
+    public static void main(String[] args) {
+        String a = "sun";
+        String b = "sun";
+        System.out.println(a.equals(b));
+        System.out.println(a == b);
+    }
+}
