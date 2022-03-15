9ed08a6878ff366f02dee6381811d7fb59de89c7
SUMMER988 <summmer206@gmail.com>
2022-03-15 11:33:18 +0800

    Exception_

---
+++ chapter12/src/com/sun/throws_/ThrowsDetail.java
@@ -0,0 +1,13 @@
+package com.sun.throws_;
+
+/**
+ * ClassName: ThrowsDetail
+ * Description:
+ * date: 2022/3/15 11:33
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class ThrowsDetail {
+}
