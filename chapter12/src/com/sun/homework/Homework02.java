4224827e4f7ccf847703172e83f583d63c3f1a64
SUMMER988 <summmer206@gmail.com>
2022-03-15 16:10:54 +0800

    Exception_

---
+++ chapter12/src/com/sun/homework/Homework02.java
@@ -0,0 +1,16 @@
+package com.sun.homework;
+
+/**
+ * ClassName: Homework02
+ * Description:
+ * date: 2022/3/15 16:10
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class Homework02 {
+    public static void main(String[] args) {
+        
+    }
+}
