bea64bd2a806befcf2db15efaf2a2e4eaea8cb72
SUMMER988 <summmer206@gmail.com>
2022-03-15 11:02:02 +0800

    Exception_

---
+++ chapter12/src/com/sun/try_/TryCatchExercise04.java
@@ -0,0 +1,13 @@
+package com.sun.try_;
+
+/**
+ * ClassName: TryCatchExercise04
+ * Description:
+ * date: 2022/3/15 11:01
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class TryCatchExercise04 {
+}
