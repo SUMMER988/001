42c4a492f2390b5901c2b2fc33256035e3be4de6
SUMMER988 <summmer206@gmail.com>
2022-03-17 00:43:36 +0800

    wrapper

---
+++ chapter13/src/com/sun/string_/StringUML.java
@@ -0,0 +1,16 @@
+package com.sun.string_;
+
+/**
+ * ClassName: StringUML
+ * Description:
+ * date: 2022/3/17 0:39
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class StringUML {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ chapter13/src/com/sun/string_/Test99.java
@@ -0,0 +1,13 @@
+package com.sun.string_;
+
+/**
+ * ClassName: Test99
+ * Description:
+ * date: 2022/3/17 0:43
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+public class Test99 {
+}
