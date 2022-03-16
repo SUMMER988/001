df87e696ce47c9ae81883c2512e7febf3645bf4b
SUMMER988 <summmer206@gmail.com>
2022-03-17 00:38:56 +0800

    wrapper

--- chapter13/src/com/sun/string_/Test.java
+++ chapter13/src/com/sun/string_/Test.java
@@ -1,25 +1,25 @@
-package com.sun.string_;
-
-import java.util.Random;
-
-/**
- * ClassName: Test
- * Description:
- * date: 2022/3/15 23:49
- *
- * @author sun
- * @version 1.0.0
- * @since JDK 1.8
- */
-@SuppressWarnings({"all"})
-public class Test {
-    public static void main(String[] args) {
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            if (i == 5) {
-                System.out.println();
-            }
-            System.out.print((int) (2 + (Math.random() * 6)));
-        }
-    }
-}
+// package com.sun.string_;
+//
+// import java.util.Random;
+//
+// /**
+//  * ClassName: Test
+//  * Description:
+//  * date: 2022/3/15 23:49
+//  *
+//  * @author sun
+//  * @version 1.0.0
+//  * @since JDK 1.8
+//  */
+// @SuppressWarnings({"all"})
+// public class Test {
+//     public static void main(String[] args) {
+//         Random random = new Random();
+//         for (int i = 0; i < 10; i++) {
+//             if (i == 5) {
+//                 System.out.println();
+//             }
+//             System.out.print((int) (2 + (Math.random() * 6)));
+//         }
+//     }
+// }
