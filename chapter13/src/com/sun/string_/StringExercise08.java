ad4a654994abbd88b30dc543e667f52b776ae0b1
SUMMER988 <summmer206@gmail.com>
2022-03-17 00:18:55 +0800

    wrapper

--- chapter13/src/com/sun/string_/StringExercise08.java
+++ chapter13/src/com/sun/string_/StringExercise08.java
@@ -13,17 +13,16 @@ public class StringExercise08 {
     public static void main(String[] args) {
         String a = "hello"; //创建 a对象
         String b = "abc";//创建 b对象
+        //老韩解读
+        //1. 先 创建一个 StringBuilder sb = StringBuilder()
+        //2. 执行  sb.append("hello");
+        //3. sb.append("abc");
+        //4. String c= sb.toString()
+        //最后其实是 c 指向堆中的对象(String) value[] -> 池中 "helloabc"
         String c = a + b;
-        StringBuilder str = new StringBuilder();
-        str.append(1);
-        str.append(1);
-        str.append(1);
-        str.append(1);
-        System.out.println(str);
-
-        // String d = "helloabc";
-        // System.out.println(c == d);//真还是假? 是false
-        // String e = "hello" + "abc";//直接看池， e指向常量池
-        // System.out.println(d == e);//真还是假? 是true
+        String d = "helloabc";
+        System.out.println(c == d);//真还是假? 是false
+        String e = "hello" + "abc";//直接看池， e指向常量池
+        System.out.println(d == e);//真还是假? 是true
     }
 }
