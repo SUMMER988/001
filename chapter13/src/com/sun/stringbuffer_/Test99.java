735c1a9838d68842b6ba8340455b6de3c33f184a
SUMMER988 <summmer206@gmail.com>
2022-03-17 11:51:34 +0800

    wrapper

--- chapter13/src/com/sun/stringbuffer_/Test99.java
+++ chapter13/src/com/sun/stringbuffer_/Test99.java
@@ -12,16 +12,29 @@ package com.sun.stringbuffer_;
 @SuppressWarnings({"all"})
 public class Test99 {
     public static void main(String[] args) {
-        String str = "YouAreWellcome";
-        StringBuffer stringBuffer = new StringBuffer(str);
+        StringBuffer s = new StringBuffer("hello");
+        s.append(',');
+        s.append("张三丰");
+        s.append("赵敏").append(100).append(true).append(10.5);
+        System.out.println(s);
+        s.delete(11, 14);
+        s.append("猪八戒").append("猪八戒").append("猪八戒").append("猪八戒").append("猪八戒");
+        System.out.println(s);
+        while (true) {
+            int delIndex = s.indexOf("猪八戒");
+            if (delIndex == -1) {
+                break;
+            }
+            s.delete(delIndex, delIndex + 3);
+        }
+        s.replace(9, 11, "周芷若");
+        System.out.println(s);
 
-        StringBuffer stringBuffer1 = new StringBuffer();
-        stringBuffer1.append(str);
-
-        StringBuffer stringBuffer2 = new StringBuffer("sun");
-        String s = stringBuffer2.toString();
-        String s1 = new String(stringBuffer2);
-        System.out.println(s1);
-
+        int indexOf = s.indexOf("张三丰");
+        System.out.println(indexOf);
+        s.insert(indexOf + "张三丰".length(), "赵敏");
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.capacity());
     }
 }
