2960cfca5c277c563764a2fedf7879aa512f3891
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-19 01:22:33 +0800

    wrapper

---
+++ chapter13/src/com/sun/homework/Homework02.java
@@ -0,0 +1,14 @@
+package com.sun.homework;
+
+/**
+ * ClassName: Homework02
+ * Description:
+ * date: 2022/3/19 1:22
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+@SuppressWarnings({"all"})
+public class Homework02 {
+}
