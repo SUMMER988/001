ff48b41c3a1352ba3ff757a31400805cd9b41ec2
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-19 00:17:39 +0800

    wrapper

--- chapter13/src/com/sun/date_/Test99.java
+++ chapter13/src/com/sun/date_/Test99.java
@@ -1,5 +1,6 @@
 package com.sun.date_;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,11 +15,15 @@ import java.util.Date;
  */
 @SuppressWarnings({"all"})
 public class Test99 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Date date = new Date();
         System.out.println(date);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println(simpleDateFormat.format(date));
 
+        String s = "1996-01-01 10:20:30";
+        Date parse = simpleDateFormat.parse(s);
+        System.out.println(parse);
+        System.out.println(simpleDateFormat.format(parse));
     }
 }
