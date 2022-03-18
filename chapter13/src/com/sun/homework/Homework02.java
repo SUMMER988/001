40c8acf374f147658cfa4c9d12cdb7b85d802e66
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-19 02:01:30 +0800

    wrapper

--- chapter13/src/com/sun/homework/Homework02.java
+++ chapter13/src/com/sun/homework/Homework02.java
@@ -1,5 +1,7 @@
 package com.sun.homework;
 
+import java.util.Scanner;
+
 /**
  * ClassName: Homework02
  * Description:
@@ -11,4 +13,53 @@ package com.sun.homework;
  */
 @SuppressWarnings({"all"})
 public class Homework02 {
+    public static void main(String[] args) {
+        init();
+    }
+
+    public static void init() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1");
+        String name = scanner.nextLine();
+        System.out.println("2");
+        String pwd = scanner.nextLine();
+        System.out.println("3");
+        String mail = scanner.nextLine();
+        try {
+            userRegister(name, pwd, mail);
+            System.out.println("注册成功");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+    }
+
+    public static void userRegister(String name, String pwd, String mail) {
+        if (name.length() < 2 || name.length() > 4) {
+            throw new RuntimeException("用户名长度有误");
+        }
+        if (pwd.length() != 6 || !isDigital(pwd)) {
+            throw new RuntimeException("密码长度有误");
+        }
+        if (!(mail.contains("@") && mail.contains(".")
+                && mail.indexOf("@") < mail.indexOf("."))) {
+            throw new RuntimeException("邮箱格式有误");
+        }
+
+    }
+    public static boolean isDigital(String pwd) {
+        long startTime = 0L;
+        long endTime = 0L;
+        startTime = System.nanoTime();
+        char[] chars = pwd.toCharArray();
+        for (char ch : chars) {
+            if (ch < '0' || ch > '9') {
+                return false;
+            }
+        }
+        endTime = System.nanoTime();
+        // System.out.println("TotalTime is " + (endTime - startTime));
+        return true;
+    }
 }
+
--- chapter13/src/com/sun/homework/Test99.java
+++ chapter13/src/com/sun/homework/Test99.java
@@ -12,35 +12,6 @@ package com.sun.homework;
 @SuppressWarnings({"all"})
 public class Test99 {
     public static void main(String[] args) {
-        String str = "ABCDEFG";
-        System.out.println("===交换前===");
-        System.out.println("str = " + str);
-        try {
-            str = reverse(str, 2, 5);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return;
-        }
-        System.out.println("===交换后===");
-        System.out.println("str = " + str);
-
-    }
-    public static String reverse(String str, int start, int end) {
-        if (str == null) {
-            throw new RuntimeException("字符串不能为空");
-        }
-        if (!(start >= 0 && end < str.length() && start < end)) {
-            throw new RuntimeException("参数设置有误");
-        }
-        char[] chars = str.toCharArray();
-        while (start < end) {
-            swap(chars, start++, end--);
-        }
-        return new String(chars);
-    }
-    public static void swap(char[] chars, int left, int right) {
-        char temp = chars[left];
-        chars[left] = chars[right];
-        chars[right] = temp;
+        
     }
 }
