ecc91f4a4da26a9d31f33bfb590c69b023b63cbd
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-20 11:12:43 +0800

    collection_

---
+++ chapter14/src/com/sun/collection_/CollectionExercise.java
@@ -0,0 +1,17 @@
+package com.sun.collection_;
+
+/**
+ * ClassName: CollectionExercise
+ * Description:
+ * date: 2022/3/20 11:12
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+@SuppressWarnings({"all"})
+public class CollectionExercise {
+    public static void main(String[] args) {
+        
+    }
+}
