0afc19b1504596f5ad4bb37d066cd4d57070f57f
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-20 11:34:37 +0800

    collection_

---
+++ chapter14/src/com/sun/list_/ListMethod.java
@@ -0,0 +1,17 @@
+package com.sun.list_;
+
+/**
+ * ClassName: ListMethod
+ * Description:
+ * date: 2022/3/20 11:34
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+@SuppressWarnings({"all"})
+public class ListMethod {
+    public static void main(String[] args) {
+        
+    }
+}
