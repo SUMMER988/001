4e96a3c2ba98ad76ffc4afd392d10fce869f2473
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-20 09:41:16 +0800

    collection_

---
+++ chapter14/src/com/sun/collection_/Collection_.java
@@ -0,0 +1,17 @@
+package com.sun.collection_;
+
+/**
+ * ClassName: Collection_
+ * Description:
+ * date: 2022/3/20 9:38
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+@SuppressWarnings({"all"})
+public class Collection_ {
+    public static void main(String[] args) {
+
+    }
+}
