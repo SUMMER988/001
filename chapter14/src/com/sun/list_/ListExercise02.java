178ec6a0059a8ea5e99e2a2f79580d88244b5a12
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-20 12:17:46 +0800

    list_

---
+++ chapter14/src/com/sun/list_/ListExercise02.java
@@ -0,0 +1,17 @@
+package com.sun.list_;
+
+/**
+ * ClassName: ListExercise02
+ * Description:
+ * date: 2022/3/20 12:17
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+@SuppressWarnings({"all"})
+public class ListExercise02 {
+    public static void main(String[] args) {
+        
+    }
+}
