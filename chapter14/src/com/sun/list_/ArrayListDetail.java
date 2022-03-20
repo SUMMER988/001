0ad56a1d36858b1b3fccdcfe7f10ad802feff38b
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-20 16:12:49 +0800

    list_

--- chapter14/src/com/sun/list_/ArrayListDetail.java
+++ chapter14/src/com/sun/list_/ArrayListDetail.java
@@ -1,8 +1,10 @@
 package com.sun.list_;
 
+import java.util.ArrayList;
+
 /**
  * ClassName: ArrayListDetail
- * Description:
+ * Description: ArrayList 线程不安全，没有 synchronized
  * date: 2022/3/20 13:11
  *
  * @author sun
@@ -12,6 +14,21 @@ package com.sun.list_;
 @SuppressWarnings({"all"})
 public class ArrayListDetail {
     public static void main(String[] args) {
-        
+
+        //ArrayList 是线程不安全的, 可以看源码 没有 synchronized
+        /*
+            public boolean add(E e) {
+                ensureCapacityInternal(size + 1);  // Increments modCount!!
+                elementData[size++] = e;
+                return true;
+            }
+         */
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(null);
+        arrayList.add("jack");
+        arrayList.add(null);
+        arrayList.add("hsp");
+        System.out.println(arrayList);
     }
 }
+
