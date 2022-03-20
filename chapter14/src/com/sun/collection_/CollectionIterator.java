ceb2670b4f3c522de38d406d004d2940bc94174e
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-20 11:00:20 +0800

    collection_

--- chapter14/src/com/sun/collection_/CollectionIterator.java
+++ chapter14/src/com/sun/collection_/CollectionIterator.java
@@ -2,6 +2,7 @@ package com.sun.collection_;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * ClassName: CollectionIterator
@@ -14,28 +15,81 @@ import java.util.Collection;
  */
 @SuppressWarnings({"all"})
 public class CollectionIterator {
+    @SuppressWarnings({"all"})
     public static void main(String[] args) {
 
         Collection col = new ArrayList();
 
-        col.add(Book.getInstance("三国演义", "罗贯中", 10.1));
-        col.add(Book.getInstance("小李飞刀", "古龙", 5.1));
-        col.add(Book.getInstance("红楼梦", "曹雪芹", 34.6));
-        System.out.println(col);
-        for (Object obj : col) {
-            System.out.println(obj);
+        col.add(new Book("三国演义", "罗贯中", 10.1));
+        col.add(new Book("小李飞刀", "古龙", 5.1));
+        col.add(new Book("红楼梦", "曹雪芹", 34.6));
+
+        //System.out.println("col=" + col);
+        //现在老师希望能够遍历 col集合
+        //1. 先得到 col 对应的 迭代器
+        Iterator iterator = col.iterator();
+        //2. 使用while循环遍历
+//        while (iterator.hasNext()) {//判断是否还有数据
+//            //返回下一个元素，类型是Object
+//            Object obj = iterator.next();
+//            System.out.println("obj=" + obj);
+//        }
+        //老师教大家一个快捷键，快速生成 while => itit
+        //显示所有的快捷键的的快捷键 ctrl + j
+        while (iterator.hasNext()) {
+            Object obj = iterator.next();
+            System.out.println("obj=" + obj);
 
         }
+        //3. 当退出while循环后 , 这时iterator迭代器，指向最后的元素
+        //   iterator.next();//NoSuchElementException
+        //4. 如果希望再次遍历，需要重置我们的迭代器
+        iterator = col.iterator();
+        System.out.println("===第二次遍历===");
+        while (iterator.hasNext()) {
+            Object obj = iterator.next();
+            System.out.println("obj=" + obj);
+
+        }
+
     }
 }
 
 class Book {
-    private final String name;
-    private final String author;
+    private String name;
+    private String author;
     private double price;
+
+    public Book(String name, String author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
     public void setPrice(double price) {
         this.price = price;
     }
+
     @Override
     public String toString() {
         return "Book{" +
@@ -43,22 +97,5 @@ class Book {
                 ", author='" + author + '\'' +
                 ", price=" + price +
                 '}';
-    }
-    private Book(String name, String author, double price) {
-        this.name = name;
-        this.author = author;
-        this.price = price;
-    }
-    public static Book getInstance(String name, String author, double price) {
-        return new Book(name, author, price);
-    }
-    public String getName() {
-        return name;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public double getPrice() {
-        return price;
     }
 }
