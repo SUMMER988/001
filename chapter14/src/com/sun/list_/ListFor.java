fe4f7b42c39b4035a2379afb1cd7a2df2dd5d586
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-20 12:11:44 +0800

    list_

---
+++ chapter14/src/com/sun/list_/ListFor.java
@@ -0,0 +1,17 @@
+package com.sun.list_;
+
+/**
+ * ClassName: ListFor
+ * Description:
+ * date: 2022/3/20 12:11
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+@SuppressWarnings({"all"})
+public class ListFor {
+    public static void main(String[] args) {
+        
+    }
+}
