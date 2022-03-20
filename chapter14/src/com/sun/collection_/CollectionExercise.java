425ea94770418cbeba061da304d3115ad1460460
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-20 11:28:09 +0800

    collection_

--- chapter14/src/com/sun/collection_/CollectionExercise.java
+++ chapter14/src/com/sun/collection_/CollectionExercise.java
@@ -1,5 +1,9 @@
 package com.sun.collection_;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * ClassName: CollectionExercise
  * Description:
@@ -12,6 +16,53 @@ package com.sun.collection_;
 @SuppressWarnings({"all"})
 public class CollectionExercise {
     public static void main(String[] args) {
-        
+        List list = new ArrayList();
+        list.add(Dog.getInstance("A", 3));
+        list.add(Dog.getInstance("B", 1));
+        list.add(Dog.getInstance("C", 2));
+        System.out.println(list);
+        for (Object dog : list) {
+            System.out.println(dog);
+        }
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Object dog = iterator.next();
+            System.out.println(dog);
+        }
+        iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Object dog = iterator.next();
+
+            System.out.println(dog);
+        }
+
+    }
+}
+
+class Dog {
+    private final String name;
+    private int age;
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+    private Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public static Dog getInstance(String name, int age) {
+        return new Dog(name, age);
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
     }
 }
