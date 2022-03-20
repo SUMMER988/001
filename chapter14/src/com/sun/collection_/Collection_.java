606caad223abdbdbd2c1c7cab33234365c7a4b8e
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-20 09:49:21 +0800

    collection_

--- chapter14/src/com/sun/collection_/Collection_.java
+++ chapter14/src/com/sun/collection_/Collection_.java
@@ -1,5 +1,9 @@
 package com.sun.collection_;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
 /**
  * ClassName: Collection_
  * Description:
@@ -11,7 +15,22 @@ package com.sun.collection_;
  */
 @SuppressWarnings({"all"})
 public class Collection_ {
+    @SuppressWarnings({"all"})
     public static void main(String[] args) {
+        //老韩解读
+        //1. 集合主要是两组(单列集合 , 双列集合)
+        //2. Collection 接口有两个重要的子接口 List Set , 他们的实现子类都是单列集合
+        //3. Map 接口的实现子类 是双列集合，存放的 K-V
+        //4. 把老师梳理的两张图记住
+        //Collection
+        //Map
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("jack");
+        arrayList.add("tom");
+        System.out.println(arrayList);
 
+        HashMap hashMap = new HashMap();
+        hashMap.put("NO1", "北京");
+        hashMap.put("NO2", "上海");
     }
 }
