1d65a0193f8b98e8fb90cea4c83e4f3d6558edc5
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-30 00:06:22 +0800

    JAVA

---
+++ chapter14/src/com/sun/set_/LinkedHashSetSource.java
@@ -0,0 +1,17 @@
+package com.sun.set_;
+
+/**
+ * ClassName: LinkedHashSetSource
+ * Description:
+ * date: 2022/3/30 0:06
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+@SuppressWarnings({"all"})
+public class LinkedHashSetSource {
+    public static void main(String[] args) {
+        
+    }
+}
--- chapter14/src/com/sun/set_/Test99.java
+++ chapter14/src/com/sun/set_/Test99.java
@@ -1,9 +1,11 @@
 package com.sun.set_;
 
+import java.util.Random;
+
 /**
  * ClassName: Test99
  * Description:
- * date: 2022/3/20 18:07
+ * date: 2022/3/20 18:00
  *
  * @author sun
  * @version 1.0.0
@@ -11,7 +13,56 @@ package com.sun.set_;
  */
 @SuppressWarnings({"all"})
 public class Test99 {
-    public static void main(String[] args) {
+}
 
+//父类
+class BBB {
+    public static long time = System.currentTimeMillis();
+
+    static {
+        timeElapsed();//设置延时
+        System.out.println("B类静态块" + +System.currentTimeMillis());
+    }
+
+    public static BBB t1 = new BBB();
+    public static BBB t2 = new BBB();
+    public BBB() {
+        super();
+        timeElapsed();//设置延时
+        System.out.println("B类构造块" + System.currentTimeMillis());
+    }
+    // 延时方法
+    public static void timeElapsed() {
+        for (int i = 0; i < 10000000; i++) {
+            int a = new Random(100).nextInt();
+            int b = new Random(100).nextInt();
+            a = a + b;
+        }
     }
 }
+
+//子类
+class AAA extends BBB {
+    public static long time = System.currentTimeMillis();
+
+    static {
+        timeElapsed();//设置延时
+        System.out.println("A类静态块");
+    }
+
+    public static AAA t1 = new AAA();
+    public static AAA t2 = new AAA();
+    public AAA() {
+        super();
+        timeElapsed();//设置延时
+        System.out.println("A类构造块");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
+        AAA t = new AAA();
+    }
+}
+
