4ed8de5705237c8cac4dade11ab10d8b242c6d74
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-03-31 01:45:40 +0800

    JAVA

--- chapter14/src/com/sun/set_/Test99.java
+++ chapter14/src/com/sun/set_/Test99.java
@@ -1,7 +1,5 @@
 package com.sun.set_;
 
-import java.util.Random;
-
 /**
  * ClassName: Test99
  * Description:
@@ -13,56 +11,29 @@ import java.util.Random;
  */
 @SuppressWarnings({"all"})
 public class Test99 {
-}
-
-//父类
-class BBB {
-    public static long time = System.currentTimeMillis();
-
-    static {
-        timeElapsed();//设置延时
-        System.out.println("B类静态块" + +System.currentTimeMillis());
-    }
-
-    public static BBB t1 = new BBB();
-    public static BBB t2 = new BBB();
-    public BBB() {
-        super();
-        timeElapsed();//设置延时
-        System.out.println("B类构造块" + System.currentTimeMillis());
-    }
-    // 延时方法
-    public static void timeElapsed() {
-        for (int i = 0; i < 10000000; i++) {
-            int a = new Random(100).nextInt();
-            int b = new Random(100).nextInt();
-            a = a + b;
-        }
-    }
-}
-
-//子类
-class AAA extends BBB {
-    public static long time = System.currentTimeMillis();
-
-    static {
-        timeElapsed();//设置延时
-        System.out.println("A类静态块");
-    }
-
-    public static AAA t1 = new AAA();
-    public static AAA t2 = new AAA();
-    public AAA() {
-        super();
-        timeElapsed();//设置延时
-        System.out.println("A类构造块");
-    }
-
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            System.out.println(i);
-        }
-        AAA t = new AAA();
+        new AA().f1();
+        new AA().f1();
+
     }
 }
 
+class AA {
+    private static int num;
+
+    static {
+        num++;
+        System.out.println("static codeBlock");
+    }
+    {
+        System.out.println("average codeBlock");
+    }
+
+    public AA() {
+        System.out.println("AA()");
+    }
+
+    public static void f1() {
+        System.out.println("f1()");
+    }
+}
