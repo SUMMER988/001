12c9a31803fe6a43d0a0cd68e8fd2e0e63c4e8c7
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-04-02 00:48:05 +0800

    JAVA

---
+++ chapter14/src/com/sun/map_/MapExercise.java
@@ -0,0 +1,17 @@
+package com.sun.map_;
+
+/**
+ * ClassName: MapExercise
+ * Description:
+ * date: 2022/4/2 0:47
+ *
+ * @author sun
+ * @version 1.0.0
+ * @since JDK 1.8
+ */
+@SuppressWarnings({"all"})
+public class MapExercise {
+    public static void main(String[] args) {
+        
+    }
+}
