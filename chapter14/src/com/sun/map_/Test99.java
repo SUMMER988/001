6eca6519efa54abacf59150c44b9272a96cc2250
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-04-14 23:50:11 +0800

    JAVA

--- chapter14/src/com/sun/map_/Test99.java
+++ chapter14/src/com/sun/map_/Test99.java
@@ -13,9 +13,34 @@ package com.sun.map_;
 public class Test99 {
     public static void main(String[] args) {
         int[] nums = {9, 8, 7, 6, 5, 4, 3};
-        Solution.bubbleSort(nums);
-        Solution.printArr(nums);
+        quickSort(nums, 0, nums.length - 1);
 
     }
+
+    public static void quickSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int pivot = partition(nums, left, right);
+            quickSort(nums, left, pivot - 1);
+            quickSort(nums, pivot + 1, right);
+        }
+    }
+
+    public static int partition(int[] nums, int left, int right) {
+        int pivot = nums[left];
+        while (left < right) {
+            while (left < right && nums[right] >= pivot) {
+                right--;
+            }
+            nums[left] = nums[right];
+            while (left < right && nums[left] <= pivot) {
+                left++;
+            }
+            nums[right] = nums[left];
+        }
+        nums[left] = pivot;
+        return left;
+    }
 }
+
+
 
